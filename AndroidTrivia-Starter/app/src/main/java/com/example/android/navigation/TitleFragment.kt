4319d276514b8accad817dabf4750539788c976e
aisnote <aisnote@gmail.com>
2019-11-02 11:28:36 -0700

    add loggin for monitoring the lifecycle of fragment

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,7 +1,9 @@
 package com.example.android.navigation
 
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.TextView
@@ -15,8 +17,12 @@ import com.example.android.navigation.databinding.FragmentTitleBinding
  */
 class TitleFragment : Fragment() {
 
+    //private val tag = "TitleFragment"
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        //Log.i(tag, "onCreateView Called")
+
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title,
                 container, false)
 
@@ -27,6 +33,44 @@ class TitleFragment : Fragment() {
 
         setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        Log.i("TitleFragment", "onAttach called")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("TitleFragment", "onCreate called")
+    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        Log.i("TitleFragment", "onActivityCreated called")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.i("TitleFragment", "onStart called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i("TitleFragment", "onResume called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i("TitleFragment", "onPause called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i("TitleFragment", "onStop called")
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.i("TitleFragment", "onDestroyView called")
+    }
+    override fun onDetach() {
+        super.onDetach()
+        Log.i("TitleFragment", "onDetach called")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
