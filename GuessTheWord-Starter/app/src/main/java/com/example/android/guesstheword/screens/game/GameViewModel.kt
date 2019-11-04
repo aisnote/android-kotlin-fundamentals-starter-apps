edfabc2520b8506477db7077de443dc9861af5e7
aisnote <aisnote@gmail.com>
2019-11-03 21:25:45 -0800

    finished: https://codelabs.developers.google.com/codelabs/kotlin-android-training-live-data-transformations/#8

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,16 +1,44 @@
 package com.example.android.guesstheword.screens.game
 
+import android.os.CountDownTimer
+import android.text.format.DateUtils
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
+
+    companion object {
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1000L
+        private const val COUNTDOWN_TIME = 10000L
+    }
+
+    private val _currentTime = MutableLiveData<Long>()
+    val currentTime: LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime) {
+        time -> DateUtils.formatElapsedTime(time)
+    }
+
+
+
+    private val timer: CountDownTimer
 
     // The current word
     private val _word = MutableLiveData<String>()
     val word: LiveData<String>
         get() = _word
+
+    val wordHint = Transformations.map(word) { word ->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters" +
+                "\nThe letter at position " + randomPosition + " is " +
+                word.get(randomPosition - 1).toUpperCase()
+    }
 
     // The current score
     private val _score = MutableLiveData<Int>()
@@ -32,12 +60,27 @@ class GameViewModel: ViewModel() {
 
         resetList()
         nextWord()
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+            override fun onTick(p0: Long) {
+                _currentTime.value = p0 / ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+                // resetList()
+            }
+        }
+
+        timer.start()
     }
 
     override fun onCleared() {
         super.onCleared()
 
-        Log.i("GameViewModel", "GameViewModel destroyed!")
+        timer.cancel()
+        // Log.i("GameViewModel", "GameViewModel destroyed!")
     }
 
     fun onGameFinish() {
